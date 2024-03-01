299a46f3d98126991e7153e215b88ab6d198a3cc
taylorgrant <taylorgrant38@gmail.com>
2024-03-01 15:04:47 -0800

    function for shiny tab navigation

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,14 @@
+## function to convert from one shiny tab to another ##
+convertMenuItem <- function(mi,tabName) {
+  mi$children[[1]]$attribs['data-toggle']="tab"
+  mi$children[[1]]$attribs['data-value'] = tabName
+  if(length(mi$attribs$class)>0 && mi$attribs$class=="treeview"){
+    mi$attribs$class=NULL
+  }
+  mi
+}
+
+
 remove_html <- function(string) {
   return(gsub("<.*?>", "", string))
 }
