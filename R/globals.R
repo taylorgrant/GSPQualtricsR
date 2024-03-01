ef5bba16223f3a634ff1421b0b10a7e62308b702
taylorgrant <taylorgrant38@gmail.com>
2024-03-01 15:03:36 -0800

    globals

--- R/globals.R
+++ R/globals.R
@@ -2,4 +2,4 @@ utils::globalVariables(c("Cohorts", "Generations", "ImportId", "block", "export_
                        "question_id", "question_order", "question_text", "question_type", "selector_type",
                        "type", "na_all", "sublabel_clean", "svy_summary", "group_label",
                        "proportion", "var_label", "var_num", "s1", "s2", "sub_label", "n", "variable",
-                       "creationDate", "age"))
+                       "creationDate", "age", "group_sub", "question_sub"))
