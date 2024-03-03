33f4b7fa829d510769ec049231419998bc097f56
taylorgrant <taylorgrant38@gmail.com>
2024-03-02 16:38:56 -0800

    adding filter capability prior to summarizing

--- R/get_responses.R
+++ R/get_responses.R
@@ -11,6 +11,7 @@
 #' @param qsub Any sub-label or extra definition pulled from the API
 #' @param resps Vector of all possible questions respondents could answer
 #' @param selector Selector type - if Likert the app will allow for top/bottom box
+#' @param filters List containing any Q columns to filter and the responses to keep
 #'
 #' @return Data frame with with all grouped data and the survey proportion
 #' @export
@@ -19,13 +20,22 @@
 #'\dontrun{
 #' na
 #' }
-get_responses <- function(data, group, gsub, var, qname, qsub, selector, resps) {
+get_responses <- function(data, group, gsub, var, qname, qsub, selector, resps, filters) {
   quo_var <- rlang::sym(var)
 
-  tmp <- data$svy |>
-    # can't get logic, but can drop anyone that didn't answer all questions tied to a specific ID
-    dplyr::mutate(na_all = (rowSums(dplyr::across(dplyr::matches(resps), is.na))) == length(resps)) |>
-    dplyr::filter(na_all == FALSE)
+  if (is.na(filters$filterQ)) {
+    tmp <- data$svy |>
+      # can't get logic, but can drop anyone that didn't answer all questions tied to a specific ID
+      dplyr::mutate(na_all = (rowSums(dplyr::across(dplyr::matches(resps), is.na))) == length(resps)) |>
+      dplyr::filter(na_all == FALSE)
+  } else {
+    quo_filter <- rlang::sym(filters$filterQ)
+    tmp <- data$svy |>
+      # can't get logic, but can drop anyone that didn't answer all questions tied to a specific ID
+      dplyr::mutate(na_all = (rowSums(dplyr::across(dplyr::matches(resps), is.na))) == length(resps)) |>
+      dplyr::filter(na_all == FALSE) |>
+      dplyr::filter(!!quo_filter %in% filters$filter_choices)
+  }
 
   if (is.na(group)) {
     # --- Single Variable --- #
