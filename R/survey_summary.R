c93199a621e952736ef43baaf6499799b28e9c3e
taylorgrant <taylorgrant38@gmail.com>
2024-04-15 09:41:39 -0700

    was breaking with empty cells

--- R/survey_summary.R
+++ R/survey_summary.R
@@ -28,29 +28,29 @@ survey_summary <- function(tbl, block, group, gsub, var, qname, qsub, selector) 
                     variable = var,
                     var_label = trimws(gsub("[\r\n\t]", "", haven::as_factor(!!rlang::sym(var)))),
                     var_label = haven::as_factor(var_label),
-                    var_num = as.numeric(!!rlang::sym(var)),
+                    var_num = as.integer(!!rlang::sym(var)),
                     total_n = attr(tbl, "total_n")) |>
       dplyr::select(-!!rlang::sym(var)) |>
       dplyr::mutate(question_text = qname,
                     question_sub = qsub,
                     selector = selector) |>
       dplyr::filter(!is.na(var_label)) |>
-      dplyr::relocate(c(question_text, question_sub, var_num, var_label), .before = proportion)
+      dplyr::relocate(c(question_text, question_sub, var_label), .before = proportion)
   } else {
     tbl <- tbl |>
-      dplyr::summarise(proportion = srvyr::survey_mean(vartype = "ci"),
+      dplyr::summarise(proportion = srvyr::survey_mean(vartype = "ci", na.rm = FALSE),
                        n = srvyr::unweighted(dplyr::n())) |>
       dplyr::mutate(block = block,
                     variable = var,
                     group_variable = group,
                     var_label = trimws(gsub("[\r\n\t]", "", haven::as_factor(!!rlang::sym(var)))),
                     var_label = haven::as_factor(var_label),
-                    var_num = as.numeric(!!rlang::sym(var)),
+                    var_num = as.integer(!!rlang::sym(var)),
                     group_label = trimws(gsub("[\r\n\t]", "", haven::as_factor(!!rlang::sym(group)))),
                     group_label = haven::as_factor(group_label),
                     total_n = attr(tbl, "total_n")) |>
-      dplyr::select(-!!rlang::sym(var)) |>
       dplyr::ungroup() |>
+      dplyr::select(-!!rlang::sym(var)) |>
       dplyr::select(-!!rlang::sym(group)) |>
       dplyr::mutate(question_text = qname,
                     question_sub = qsub,
@@ -58,7 +58,7 @@ survey_summary <- function(tbl, block, group, gsub, var, qname, qsub, selector) 
                     selector = selector) |>
       dplyr::filter(!is.na(var_label)) |>
       dplyr::filter(!is.na(group_label)) |>
-      dplyr::relocate(c(question_text, question_sub, group_label, group_sub, var_num, var_label), .before = proportion)
+      dplyr::relocate(c(question_text, question_sub, group_label, group_sub, var_label), .before = proportion)
   }
   tbl
 }
